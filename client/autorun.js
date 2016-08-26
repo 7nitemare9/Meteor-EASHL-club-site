836ae84155b7a825471e74002df556b43b9bf870
Andreas Östlund <mr_ace@hem.utfors.se>
2016-08-26 12:47:00 +0200

    turned autorun back on

--- client/autorun.js
+++ client/autorun.js
@@ -1,20 +1,20 @@
-// Tracker.autorun(function() {
-//   if (Meteor.user()) {
-//     if (FlowRouter.current().path === '/login') {
-//       if(Meteor.user().profile.name || Meteor.user().profile.gamertag) {
-//         if (Roles.userIsInRole(Meteor.user(), ['Team-member'])) {
-//           FlowRouter.go('/teamchat');
-//         } else {
-//           FlowRouter.go('/');
-//         }
-//       }else{
-//         FlowRouter.go('/user');
-//       }
-//     }
-//   }
-//   if (!Meteor.user()) {
-//     if (FlowRouter.current().path === '/signout') {
-//       FlowRouter.go('/');
-//     }
-//   }
-// });
+Tracker.autorun(function() {
+  if (Meteor.user()) {
+    if (FlowRouter.current().path === '/login') {
+      if(Meteor.user().profile.name || Meteor.user().profile.gamertag) {
+        if (Roles.userIsInRole(Meteor.user(), ['Team-member'])) {
+          FlowRouter.go('/teamchat');
+        } else {
+          FlowRouter.go('/');
+        }
+      }else{
+        FlowRouter.go('/user');
+      }
+    }
+  }
+  if (!Meteor.user()) {
+    if (FlowRouter.current().path === '/signout') {
+      FlowRouter.go('/');
+    }
+  }
+});

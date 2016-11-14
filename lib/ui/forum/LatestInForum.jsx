a03a0fd5b79636dea6b51575663a32a52e321f0d
Andreas Ostlund <andreasostlund@Andreass-iMac.local>
2016-11-14 18:02:31 +0100

    removed bootstrap from latest in forum

--- lib/ui/forum/LatestInForum.jsx
+++ lib/ui/forum/LatestInForum.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
+import Box from '../commonCSS/box.js';
+import Colors from '../commonCSS/colors.js';
 
 export default class LatestInForum extends TrackerReact(Component) {
   constructor() {
     super();
     this.state = {isClient: false};
     this.style = {
-      width: '310px',
-      margin: '10px 0'
+      box: Object.assign(Box.box, {
+        width: '310px',
+        margin: '10px 0'
+      }),
+      ul: {
+        margin: '0px'
+      },
+      li: {
+        even: {
+          height: '22px',
+          padding: '2px 15px 0px 15px',
+          background: Colors.grey,
+          border: 'none',
+          minWidth: '200px',
+          display: 'flex',
+          justifyContent: 'space-between'
+        },
+        odd: {
+          height: '22px',
+          padding: '2px 15px 0px 15px',
+          background: Colors.darkGrey,
+          border: 'none',
+          minWidth: '200px',
+          display: 'flex',
+          justifyContent: 'space-between'
+        }
+      }
     }
   }
 
@@ -31,28 +58,30 @@ export default class LatestInForum extends TrackerReact(Component) {
     empty = empty.slice(0, 5 - this.getLatestThreads().length);
     return (
       // <div className="col-lg-3 b_column col-md-4 col-sm-6 col-xs-6">
-        <div className="b_box latest-forum" style={this.style}>
+        <div style={this.style.box} >
           <div className="b_header">
             <img src="/assets/forumposts.png" />
           </div>
-          {this.getLatestThreads().map(thread => {
-            return (
-              <li key={thread._id} className="list-group-item">
-                <a href={`/forum/thread/${thread._id}`}>
-                  <p>{thread.title}</p>
-                </a>
-              </li>
-            )
-          })}
-          {empty.map(x =>  {
-            return (
-              <li key={x} className="list-group-item">
-                <a href="#">
-                  <p></p>
-                </a>
-              </li>
-            )
-          })}
+          <ul style={this.style.ul}>
+            {this.getLatestThreads().map((thread, idx) => {
+              return (
+                <li key={thread._id} style={idx % 2 == 0 ? this.style.li.even : this.style.li.odd}>
+                  <a href={`/forum/thread/${thread._id}`}>
+                    <p>{thread.title}</p>
+                  </a>
+                </li>
+              )
+            })}
+            {empty.map((x, idx) =>  {
+              return (
+                <li key={x} style={idx % 2 == 0 ? this.style.li.even : this.style.li.odd}>
+                  <a href="#">
+                    <p></p>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
         </div>
       // </div>
     )

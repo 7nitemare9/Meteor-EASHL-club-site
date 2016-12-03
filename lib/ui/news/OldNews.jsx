0194df0075dde963f4a0442d6ce2e69fb0c21d7c
Andreas Östlund <mr_ace@hem.utfors.se>
2016-12-03 20:42:14 +0100

    unCSS and Flexbox OldNews

--- lib/ui/news/OldNews.jsx
+++ lib/ui/news/OldNews.jsx
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import News from './News.jsx';
 import SmallNewsBox from './SmallNewsBox.jsx';
 import * as NewsHelper from '../helpers/NewsHelper.js';
+import Box from '../commonCSS/box.js';
 
 export default class OldNews extends Component {
+    constructor() {
+        super();
+        this.style = {
+            box: Box.box,
+            visibleLg: {
+                display: 'none',                
+                '@media (min-width: 1341px)': {
+                    display: 'block',
+                }
+            },
+            smallNewsContainer: {
+                display: 'flex',
+                justifyContent: 'space-between',
+                flexWrap: 'wrap'
+            },
+            archiveButton: {
+                marginTop: '20px'
+            },
+            archiveImage: {
+                height: '22px',
+                background: 'url(assets/archive.png) no-repeat center'
+            }
+        }
+    }
 
     render() {
         return (
-            <div className="border-box-set">
+            <div style={this.style.smallNewsContainer}>
                 {this.props.newsData.slice(5, 11).map((data) => {
                     return (
-                      <SmallNewsBox key={data._id} news={data} class="visible-lg"/>
+                      <SmallNewsBox key={data._id} news={data} class={this.style.visibleLg} />
                 )})}
-                <div className="b_box archive-box">
+                <div style={{...this.style.box, ...this.style.archiveButton}}>
                     <a href="/news/archive/0">
-                        <div className="col-lg-12 archive-image"></div>
+                        <div style={this.style.archiveImage}></div>
                     </a>
                 </div>
             </div>
--- lib/ui/news/SmallNewsBox.jsx
+++ lib/ui/news/SmallNewsBox.jsx
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import * as NewsHelper from '../helpers/NewsHelper.js';
+import Box from '../commonCSS/box.js';
+import Colors from '../commonCSS/colors.js';
 
-export default class SmallNewsBox extends Component {
+class SmallNewsBox extends Component {
 
   render() {
-    let style = Object.assign({padding: "10px 5px 0px 5px"}, this.props.style);
+    let style = {
+      box: {
+        ...Box.box        
+      },
+      newsCard: {
+        width: '200px',
+        height: '130px',
+        marginTop: '20px'
+      },
+      newsTitle: {
+        float: 'left',
+        position: 'absolute',
+        marginTop: '-40px',
+        width: '200px',
+        height: '40px',
+        display: 'flex',
+        background: Colors.darkGrey,
+        opacity: '0.95'
+      },
+      titleText: {
+        margin: 'auto',
+        fontSize: '10px',
+        fontWeight: '600',
+        textAlign: 'center'
+      }
+    };
     if (this.props.news === undefined) {
       return (<div></div>)
     }
     return (
-      <div key={this.props.news._id} className={`col-lg-4 col-md-4 col-sm-4 col-xs-4 ${this.props.class}`}
-      style={style} >
-          <div className="b_box old-news-box" >
-              <img src={NewsHelper.getImage(this.props.news)} />
+      <div key={this.props.news._id} style={{...this.props.class, ...style.newsCard}}>
+          <div style={style.box} >
+              <img src={NewsHelper.getImage(this.props.news)} style={{width: '200px', height: '130px'}}/>
           </div>
-          <div className="old-news-title">
-              <a href={`/news/${this.props.news._id}`} >{this.props.news.title.toUpperCase()}</a>
-          </div>
+          <a href={`/news/${this.props.news._id}`} >
+            <div style={style.newsTitle}>
+              <p style={style.titleText}>{this.props.news.title.toUpperCase()}</p>
+            </div>
+          </a>
       </div>
     )
   }
 }
+export default SmallNewsBox = Radium(SmallNewsBox);

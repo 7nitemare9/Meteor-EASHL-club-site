809a340bd0c1b0eb5493cda30f4277c49146abbb
Andreas Östlund <mr_ace@hem.utfors.se>
2016-12-05 19:00:15 +0100

    news archive flexbox and uncss

--- lib/ui/news/PrevNext.jsx
+++ lib/ui/news/PrevNext.jsx
@@ -1,27 +1,73 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
+import Colors from '../commonCSS/colors.js';
 
-export default class PrevNext extends Component {
+class PrevNext extends Component {
+  constructor() {
+    super();
+    this.style = {
+      centerButtons: {
+        width: '100%',
+        margin: '20px 7px 0 7px',
+        borderTop: '3px solid' + Colors.secondary,
+        textAlign: 'center'
+      },
+      leftButton: {
+        background: Colors.grey,
+        borderRadius: '50%',
+        width: '24px',
+        height: '24px',
+        border: '0px',
+        margin: '5px 0',
+        float: 'left',
+        boxShadow: '0 2px 6px rgba(0,0,0,0.17), 0 4px 6px rgba(0,0,0,0.23)',
+        ':hover': {
+          margin: '4px 0',
+          width: '26px',
+          height: '26px',
+          boxShadow: '0 4px 12px rgba(0,0,0,0.17), 0 8px 12px rgba(0,0,0,0.23)'
+        }
+      },
+      rightButton: {
+        background: Colors.grey,
+        borderRadius: '50%',
+        width: '24px',
+        height: '24px',
+        border: '0px',
+        margin: '5px 0',
+        float: 'right',
+        boxShadow: '0 2px 6px rgba(0,0,0,0.17), 0 4px 6px rgba(0,0,0,0.23)',
+        ':hover': {
+          margin: '4px 0',
+          width: '26px',
+          height: '26px',
+          boxShadow: '0 4px 12px rgba(0,0,0,0.17), 0 8px 12px rgba(0,0,0,0.23)'
+        }
+      }
+    }
+  }
 
   render() {
     if (this.props.page == 0) {
       return (
-        <div className="center-buttons">
+        <div style={this.style.centerButtons}>
           <a href={`/news/archive/${parseInt(this.props.page) + 1}`}>
-            <button className="match-button">Next</button>
+            <div style={this.style.rightButton}><i style={{marginLeft: '2px'}} className="material-icons">chevron_right</i></div>
           </a>
         </div>
       )
     } else {
       return (
-        <div className="center-buttons">
-          <a href={`/news/archive/${this.props.page - 1}`}>
-            <button className="match-button">Previous</button>
+        <div style={this.style.centerButtons}>
+          <a key="test" href={`/news/archive/${this.props.page - 1}`}>
+            <div ref='test5' style={this.style.leftButton}><i className="material-icons">chevron_left</i></div>
           </a>
-          <a href={`/news/archive/${parseInt(this.props.page) + 1}`}>
-            <button className="match-button">Next</button>
+          <a key="test2" href={`/news/archive/${parseInt(this.props.page) + 1}`}>
+              <div key="test6" style={this.style.rightButton}><i style={{marginLeft: '2px'}} className="material-icons">chevron_right</i></div>
           </a>
         </div>
       )
     }
   }
 }
+export default PrevNext = Radium(PrevNext);
